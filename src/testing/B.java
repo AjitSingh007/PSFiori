b4c80d577a867e9fd48adca7dd712e3cab3d6a53
Ajit Singh <ajit.singh01@sap.com>
2017-05-07 16:45:13 +0530

    tesAASAS

---
+++ src/testing/B.java
@@ -0,0 +1,15 @@
+package testing;
+
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class B
+{
+    @Test
+	public void test12()
+	{
+		Reporter.log("Man", true);
+		System.out.println("Ya its working");
+	}
+}
