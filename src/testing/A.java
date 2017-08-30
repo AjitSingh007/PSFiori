9f16cf6a2c7fedf811d0a50d29b1d30518c13d5f
Ajit Singh <ajit.singh01@sap.com>
2017-08-30 08:33:07 +0530

    A

--- src/testing/A.java
+++ src/testing/A.java
@@ -1,6 +1,8 @@
 package testing;
 
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
@@ -9,6 +11,9 @@ public class A
 	@Test
 	public void test1()
 	{
+		System.setProperty("webdriver.gecko.driver","./div2/geckodriver.exe");
+		WebDriver driver= new FirefoxDriver();
+		driver.get("https://login.naukri.com/nLogin/Login.php");
 	Reporter.log("Hi",true);
 	System.out.println("Hellow");
 	}
